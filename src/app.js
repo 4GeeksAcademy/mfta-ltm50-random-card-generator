2f59a80c7540d74c099b078c5aed75a5ebea2d11
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-04-26 15:33:20 +0000

    Basic version done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.querySelector("#card");
+  const suitTop = document.querySelector("#suit-top");
+  const suitBottom = document.querySelector("#suit-bottom");
+  const number = document.querySelector("#number");
+
+  const suits = [{symbol: "♦", color: "red"}, {symbol: "♥", color: "red"}, {symbol: "♣", color: "black"}, {symbol: "♠", color: "black"}];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  suitTop.innerHTML = randomSuit.symbol;
+  suitBottom.innerHTML = randomSuit.symbol;
+  suitTop.style.color = randomSuit.color;
+  suitBottom.style.color = randomSuit.color;
+  suitBottom.style.transform = "rotate(180deg)";
+  number.innerHTML = randomNumber;
+  number.style.color = randomSuit.color;
 };
